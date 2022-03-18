8011bba9baf6e53f600e2f36a1acb1d9dfcc0ce4
Dwayne <dsutherland13@hotmail.co.uk>
2022-03-18 15:30:58 +0000

    cloudfront: setup for cloudfront added

---
+++ cloudfront.tf
@@ -0,0 +1,60 @@
+resource "aws_cloudfront_origin_access_identity" "OAI" {
+  comment = "Access ID project ${var.project_name}'s s3 bucket"
+}
+
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.s3b.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.OAI.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = false
+  comment             = "CDN for ${var.project_name}"
+  default_root_object = "index.html"
+
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  tags = {
+    project = var.project_name
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
